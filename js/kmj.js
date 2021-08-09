498114ffc213329eb082dd5adcad1f8cc9a8c3ec
Dwiki Alfian <dwikialfian92@gmail.com>
2021-08-09 16:06:31 +0700

    Initial Commit

---
+++ js/kmj.js
@@ -0,0 +1,24 @@
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = sliderparent.childNodes[i].className == "slider";
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
