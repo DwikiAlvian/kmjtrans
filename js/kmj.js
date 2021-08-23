2c01536bf2a16064a9692cf39eaff9071c21cfbe
Dwiki Alfian <dwikialfian92@gmail.com>
2021-08-23 11:31:53 +0700

    add:live-search

--- js/kmj.js
+++ js/kmj.js
@@ -7,6 +7,15 @@ const navbar = document.getElementsByClassName("navbar");
 
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
+// SLEEP FUNCTION
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 function openTab(evt, content) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -58,3 +67,50 @@ closeButton.addEventListener("click", () => {
     navStat = false;
   }
 });
+
+// NEW STUFF
+var dataLiveInput = document.querySelectorAll(".data-live input");
+var dataLiveOption = document.querySelectorAll(".data-live ul");
+var dataLiveText = document.querySelectorAll(".data-live ul li");
+let dataLiveCount, dataLiveInputCount;
+
+// OPTION INPUT
+for (dataLiveCount = 0; dataLiveCount < dataLiveText.length; dataLiveCount++) {
+  // Adding 'CLICK' function to 'data-option'
+  dataLiveText[dataLiveCount].addEventListener("click", function () {
+    // Determine value of 'data-option'
+    var dataText = this.innerHTML;
+    // Put value of 'data-option' into input tag
+    this.parentElement.parentElement.children[0].value = dataText;
+  });
+}
+
+// FILTER
+for (
+  dataLiveInputCount = 0;
+  dataLiveInputCount < dataLiveInput.length;
+  dataLiveInputCount++
+) {
+  dataLiveInput[dataLiveInputCount].addEventListener("keyup", function () {
+    for (var i = 0; i < dataLiveText.length; i++) {
+      if (
+        this.parentElement
+          .getElementsByTagName("li")
+          [i].innerText.toUpperCase()
+          .indexOf(this.value.toUpperCase()) > -1
+      ) {
+        dataLiveText[i].style.display = "";
+      } else {
+        dataLiveText[i].style.display = "none";
+      }
+    }
+  });
+  // Data Show when Input Clicked
+  dataLiveInput[dataLiveInputCount].addEventListener("focusin", function () {
+    this.parentElement.children[1].classList.add("show-data");
+  });
+  // Data Hide when Input Clicked
+  dataLiveInput[dataLiveInputCount].addEventListener("focusout", function () {
+    this.parentElement.children[1].classList.remove("show-data");
+  });
+}
